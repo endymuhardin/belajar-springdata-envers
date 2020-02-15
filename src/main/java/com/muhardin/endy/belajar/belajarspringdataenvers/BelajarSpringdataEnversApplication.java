edfd3c3e744d35bb4f353bd2a37d9ce447be26d6
Endy Muhardin <endy@muhardin.com>
2020-02-15 16:43:05 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/belajarspringdataenvers/BelajarSpringdataEnversApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.belajarspringdataenvers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringdataEnversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringdataEnversApplication.class, args);
+	}
+
+}
